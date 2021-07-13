177b22831eeb02337e951bbee02d1c536a31f827
hongsub95 <hong9506@gmail.com>
2021-07-14 02:49:45 +0900

    2021/07/14

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
+import { createStore } from "redux";
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+    switch (action.type) {
+      case ADD_TODO:
+        return [...state, { text: action.text, id: Date.now() }];
+      case DELETE_TODO:
+        return [];
+      default:
+        return state;
+    }
+  };
+  
+  const store = createStore(reducer);
+  
+  store.subscribe(() => console.log(store.getState()));
+  
+
+const onsubmit = e => {
+    e.preventDefault();
+    const toDo = input.value;
+    input.value ="";
+    createToDo(toDo);
+}
+
+form.addEventListener("submit",onsubmit)
 
